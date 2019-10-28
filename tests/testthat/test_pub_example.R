80e3a073edfc095357a323a51ff8af20a3470394
Bret Beheim <beheim@gmail.com>
2019-10-28 19:25:13 -0400

    refactored a good part of the function, but all unit tests still pass

--- tests/testthat/test_pub_example.R
+++ tests/testthat/test_pub_example.R
@@ -1,11 +1,16 @@
 
 #in this example script, we load gps data and accelerometer data from the same subject on the same day,
 #then identify the optimal time drift correction for these data. After doing so, we plot the effects of time drift correction
-#and then save an optimally synchronized data set. 
+#and then save an optimally synchronized data set.
+
+# to test:
+# no overlap entries between two input files
+# e.g.   overlap_entries <- (accel$adjusted_unixtime >= min(gps$unix_time) &
+#   accel$adjusted_unixtime <= max(gps$unix_time))
 
 test_that("sync works with no offset", {
   candidate_drift_correction <- 0
-  expect_silent(
+  expect_warning(
     out <- get_candidate_synchronization(
       gps_data = "pub_example/example_gps_data.csv",
       accel = "pub_example/example_accel_data.csv",
@@ -15,10 +20,10 @@ test_that("sync works with no offset", {
   expect_equal(nrow(out), 4560)
   expect_equal(min(out$unix_time), 1435957259)
   expect_equal(max(out$unix_time), 1436025629)
-  expect_true(abs(mean(out$speed_m_s) - 0.0884) < 1e-4)
-  expect_true(abs(var(out$speed_m_s) - 0.0528) < 1e-4)
-  expect_true(abs(mean(out$meters_in_segment) - 1.3256) < 1e-4)
-  expect_true(abs(var(out$meters_in_segment) - 11.8884) < 1e-4)
+  expect_true(abs(mean(out$speed_m_s, na.rm = TRUE) - 0.0884) < 1e-4)
+  expect_true(abs(var(out$speed_m_s, na.rm = TRUE) - 0.0528) < 1e-4)
+  expect_true(abs(mean(out$meters_in_segment, na.rm = TRUE) - 1.3256) < 1e-4)
+  expect_true(abs(var(out$meters_in_segment, na.rm = TRUE) - 11.8884) < 1e-4)
   expect_true(
     all(
       (out$adjusted_unixtime - out$unix_time) == candidate_drift_correction
@@ -29,7 +34,7 @@ test_that("sync works with no offset", {
 
 test_that("sync works with an offset of 10", {
   candidate_drift_correction <- 10
-  expect_silent(
+  expect_warning(
     out <- get_candidate_synchronization(
       gps_data = "pub_example/example_gps_data.csv",
       accel = "pub_example/example_accel_data.csv",
@@ -37,10 +42,10 @@ test_that("sync works with an offset of 10", {
     )
   )
   expect_equal(nrow(out), 4559)
-  expect_true(abs(mean(out$speed_m_s) - 0.0884) < 1e-4)
-  expect_true(abs(var(out$speed_m_s) - 0.0527) < 1e-4)
-  expect_true(abs(mean(out$meters_in_segment) - 1.3259) < 1e-4)
-  expect_true(abs(var(out$meters_in_segment) - 11.8570) < 1e-4)
+  expect_true(abs(mean(out$speed_m_s, na.rm = TRUE) - 0.0884) < 1e-4)
+  expect_true(abs(var(out$speed_m_s, na.rm = TRUE) - 0.0527) < 1e-4)
+  expect_true(abs(mean(out$meters_in_segment, na.rm = TRUE) - 1.3259) < 1e-4)
+  expect_true(abs(var(out$meters_in_segment, na.rm = TRUE) - 11.8570) < 1e-4)
   expect_true(
     all(
       (out$adjusted_unixtime - out$unix_time) == candidate_drift_correction
@@ -51,7 +56,7 @@ test_that("sync works with an offset of 10", {
 
 test_that("sync works with an offset of -10", {
   candidate_drift_correction <- (-10)
-  expect_silent(
+  expect_warning(
     out <- get_candidate_synchronization(
       gps_data = "pub_example/example_gps_data.csv",
       accel = "pub_example/example_accel_data.csv",
@@ -59,10 +64,10 @@ test_that("sync works with an offset of -10", {
     )
   )
   expect_equal(nrow(out), 4559)
-  expect_true(abs(mean(out$speed_m_s) - 0.0884) < 1e-4)
-  expect_true(abs(var(out$speed_m_s) - 0.0521) < 1e-4)
-  expect_true(abs(mean(out$meters_in_segment) - 1.3259) < 1e-4)
-  expect_true(abs(var(out$meters_in_segment) - 11.7243) < 1e-4)
+  expect_true(abs(mean(out$speed_m_s, na.rm = TRUE) - 0.0884) < 1e-4)
+  expect_true(abs(var(out$speed_m_s, na.rm = TRUE) - 0.0521) < 1e-4)
+  expect_true(abs(mean(out$meters_in_segment, na.rm = TRUE) - 1.3259) < 1e-4)
+  expect_true(abs(var(out$meters_in_segment, na.rm = TRUE) - 11.7243) < 1e-4)
   expect_true(
     all(
       (out$adjusted_unixtime - out$unix_time) == candidate_drift_correction
@@ -74,7 +79,7 @@ test_that("sync works with an offset of -10", {
 test_that("sync works with a variety of offsets", {
   candidate_drift_correction <- seq(from = 30, to = 50, by = 1)
   for (i in 1:length(candidate_drift_correction)) {
-    expect_silent(
+    expect_warning(
       out <- get_candidate_synchronization(
         gps_data = "pub_example/example_gps_data.csv",
         accel = "pub_example/example_accel_data.csv",
